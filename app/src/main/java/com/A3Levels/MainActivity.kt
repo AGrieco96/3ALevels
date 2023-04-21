1e6662eb93777506d3b9667224f947ffdebd45ca
AGrieco96 <grieco.1715585@studenti.uniroma1.it>
2023-04-21 16:07:22 +0200

    First Commit!

---
+++ app/src/main/java/com/A3Levels/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.A3Levels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    //ooooooo
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
