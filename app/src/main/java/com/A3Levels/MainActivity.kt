9abe625fab4f55b8d0ee47bdeece0848d0d77f3b
ditommaso1801802 <ditommaso.1801802@studenti.uniroma1.it>
2023-04-24 13:04:57 +0200

    FirebaseUIActivity.kt added

--- app/src/main/java/com/A3Levels/MainActivity.kt
+++ app/src/main/java/com/A3Levels/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.A3Levels
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    //ooooooo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-}// ciao
+}
