7d7b42e7ee878325b627924349100dfd8b1af657
ditommaso1801802 <ditommaso.1801802@studenti.uniroma1.it>
2023-04-27 23:15:23 +0200

    added user to firestore db in RegisterEmailActivity

--- app/src/main/java/com/A3Levels/auth/RegisterEmailActivity.kt
+++ app/src/main/java/com/A3Levels/auth/RegisterEmailActivity.kt
@@ -8,13 +8,15 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.A3Levels.databinding.RegisterEmailBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+
 
 class RegisterEmailActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: RegisterEmailBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,9 +53,10 @@ class RegisterEmailActivity : AppCompatActivity() {
 
         auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
             if(task.isSuccessful){
-                goToLogin()
-                val currentUser = auth.currentUser
-                finish()
+                val userFirebase = auth.currentUser
+                if(userFirebase != null) {
+                    addUserToDB(userFirebase)
+                }
             } else {
             // If sign in fails, display a message to the user.
             Log.w(TAG, "signInWithEmail:failure", task.exception)
@@ -66,6 +69,25 @@ class RegisterEmailActivity : AppCompatActivity() {
     private fun goToLogin(){
         val intent = Intent(this, LoginEmailActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun addUserToDB(currentUser : FirebaseUser) {
+            val db = Firebase.firestore
+
+            val user = hashMapOf(
+                "email" to currentUser.email,
+                "uid" to currentUser.uid
+            )
+
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                    goToLogin()
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
     }
 
 }
