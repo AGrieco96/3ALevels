b22a145ff9c94edcacbfa8ce8a39497e523b3511
nardoz-dev <nardomen96@gmail.com>
2023-04-29 16:01:37 +0200

    Adding Logo Transiction to MainMenu

--- app/src/main/java/com/A3Levels/MainActivity.kt
+++ app/src/main/java/com/A3Levels/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.A3Levels
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
-    //ooooooo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-}// ciao
+
+
+    val mainHandler = Handler(Looper.getMainLooper()).postDelayed({
+        val intent = Intent(this@MainActivity, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    },3000)
+
+
+
+}
