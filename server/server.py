401896ce8750a5aa1ce30a96f94b16738cdbe834
ditommaso1801802 <ditommaso.1801802@studenti.uniroma1.it>
2023-05-13 11:38:50 +0200

    add put to update game

--- server/server.py
+++ server/server.py
@@ -3,46 +3,119 @@ from firebase_admin import credentials, firestore, initialize_app
 from imageai.Classification import ImageClassification
 import os
 import base64
+import datetime
 
 # Initialize Flask App
 app = Flask(__name__)
 
 # Initialize Firestore DB
-cred = credentials.Certificate('/etc/secrets/key.json')
+cred = credentials.Certificate('./key.json')
 default_app = initialize_app(cred)
 db = firestore.client()
-todo_ref = db.collection('todos')
+games_collection = db.collection('games')
+lobby_collection = db.collection('lobbies')
 
-@app.route('/add', methods=['POST'])
-def create():
-    """
-        create() : Add document to Firestore collection with request body
-        Ensure you pass a custom ID as part of json body in post request
-        e.g. json={'id': '1', 'title': 'Write a blog post'}
-    """
+
+@app.route('/update-game', methods=['PUT'])
+def updateGame():
     try:
-        id = request.json['id']
-        todo_ref.document(id).set(request.json)
+        lobby_id = request.json['lobby_id']
+        player_id = request.json['player_id']
+        level = request.json['level']
+        time_score = request.json['time']
+        field_string_time = player_id + '.time.' + level
+
+        doc_game = games_collection.document(lobby_id)
+        doc_game.update({
+            field_string_time: time_score
+        })
+
+        doc_lobby = lobby_collection.document(lobby_id)
+        doc_l = doc_lobby.get()
+        if player_id != str(doc_l.to_dict()['player_1']):
+            other_player = str(doc_l.to_dict()['player_1'])
+        else:
+            other_player = str(doc_l.to_dict()['player_2'])
+
+        doc_g = doc_game.get()
+        time_other = doc_g.to_dict()[other_player]['time'][level]
+
+        if time_other != 0:
+            field_string_counter_player = player_id + '.counter'
+            field_string_counter_other_player = other_player + '.counter'
+            counter_player = doc_g.to_dict()[player_id]['counter'] + 1
+            counter_other_player = doc_g.to_dict()[other_player]['counter'] + 1
+            if time_other <= time_score:
+                doc_game.update({
+                    field_string_counter_other_player: counter_other_player
+                })
+            else:
+                doc_game.update({
+                    field_string_counter_player: counter_player
+                })
         return jsonify({"success": True}), 200
     except Exception as e:
-        return f"An Error Occured: {e}"
-    
+        return f"An Error Occurred: {e}"
+
+
+@app.route('/new-game', methods=['POST'])
+def createGame():
+    try:
+        lobby_id = request.json['lobby_id']
+
+        doc_lobby = lobby_collection.document(lobby_id)
+        doc = doc_lobby.get()
+        username_player1 = str(doc.to_dict()['player_1'])
+        username_player2 = str(doc.to_dict()['player_2'])
+
+        new_game = {
+            username_player1: {
+                u'counter': 0,
+                u'lobby_id': lobby_id,
+                u'time': {
+                    u'level1': 0,
+                    u'level2': 0,
+                    u'level3': 0,
+                    u'level4': 0,
+                    u'level5': 0
+                },
+            },
+            username_player2: {
+                u'counter': 0,
+                u'lobby_id': lobby_id,
+                u'time': {
+                    u'level1': 0,
+                    u'level2': 0,
+                    u'level3': 0,
+                    u'level4': 0,
+                    u'level5': 0
+                }
+            }
+        }
+        games_collection.document(lobby_id).set(new_game)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occurred: {e}"
+
+
 @app.route('/ai', methods=['POST'])
 def image_recognition():
     try:
         base64_image_string = request.json['image']
-        object = request.json['object']
+        object_str = request.json['object']
         execution_path = os.getcwd()
         prediction = ImageClassification()
         prediction.setModelTypeAsMobileNetV2()
         prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2-b0353104.pth"))
         prediction.loadModel()
 
+        timestamp = str(datetime.datetime.now())
         content = base64.b64decode(base64_image_string)
-        with open('image.jpg', 'wb') as f:
+        with open('image.jpg' + timestamp, 'wb') as f:
             f.write(content)
 
-        predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "image.jpg"), result_count=10)
+        predictions, probabilities = prediction.classifyImage(
+            os.path.join(execution_path, "image.jpg" + timestamp), result_count=10)
         string_list = []
         for s in predictions:
             if ' ' in s:
@@ -51,13 +124,14 @@ def image_recognition():
             else:
                 string_list.append(s)
 
-        if object in string_list:
-            response = "Good job! You have take a photo of a" + object
+        if object_str in string_list:
+            response = "Good job! You have take a photo of a " + object_str
         else:
-            response = "Oh no! You have take a photo of a" + predictions[0]
+            response = "Oh no! You have take a photo of a " + predictions[0]
         return jsonify(response), 200
     except Exception as e:
-        return f"An Error Occured: {e}"
+        return f"An Error Occurred: {e}"
+
 
 @app.route('/list', methods=['GET'])
 def read():
@@ -68,29 +142,16 @@ def read():
     """
     try:
         # Check if ID was passed to URL query
-        todo_id = request.args.get('id')    
+        todo_id = request.args.get('id')
         if todo_id:
-            todo = todo_ref.document(todo_id).get()
+            todo = games_collection.document(todo_id).get()
             return jsonify(todo.to_dict()), 200
         else:
-            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
+            all_todos = [doc.to_dict() for doc in games_collection.stream()]
             return jsonify(all_todos), 200
     except Exception as e:
-        return f"An Error Occured: {e}"
+        return f"An Error Occurred: {e}"
 
-@app.route('/update', methods=['POST', 'PUT'])
-def update():
-    """
-        update() : Update document in Firestore collection with request body
-        Ensure you pass a custom ID as part of json body in post request
-        e.g. json={'id': '1', 'title': 'Write a blog post today'}
-    """
-    try:
-        id = request.json['id']
-        todo_ref.document(id).update(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
 
 @app.route('/delete', methods=['GET', 'DELETE'])
 def delete():
@@ -100,11 +161,11 @@ def delete():
     try:
         # Check for ID in URL query
         todo_id = request.args.get('id')
-        todo_ref.document(todo_id).delete()
+        games_collection.document(todo_id).delete()
         return jsonify({"success": True}), 200
     except Exception as e:
-        return f"An Error Occured: {e}"
+        return f"An Error Occurred: {e}"
 
 
 if __name__ == '__main__':
-    app.run(threaded=True, host='0.0.0.0', port= 5000)
+    app.run(threaded=True, host='0.0.0.0', port=5000)
