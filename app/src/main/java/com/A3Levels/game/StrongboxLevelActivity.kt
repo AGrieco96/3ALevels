52853b22764e12414571b425dfba33cd9bf42d22
ditommaso1801802 <ditommaso.1801802@studenti.uniroma1.it>
2023-06-26 18:00:55 +0200

    sensor works

--- app/src/main/java/com/A3Levels/game/StrongboxLevelActivity.kt
+++ app/src/main/java/com/A3Levels/game/StrongboxLevelActivity.kt
@@ -1,38 +1,62 @@
 package com.A3Levels.game
 
-import android.content.ContentValues.TAG
-import androidx.appcompat.app.AppCompatActivity
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import android.view.View
-import android.view.animation.AlphaAnimation
-import android.view.animation.Animation
-import android.view.animation.LinearInterpolator
-import com.A3Levels.auth.GoogleSignInActivity
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.A3Levels.databinding.ActivityStrongboxLevelBinding
-import com.A3Levels.databinding.ActivityTestLevelBinding
-import com.A3Levels.other.RequestsHTTP
-import com.google.firebase.FirebaseApp
-import com.google.firebase.firestore.FirebaseFirestore
-import org.json.JSONObject
 
-class StrongboxLevelActivity : AppCompatActivity() {
+class StrongboxLevelActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var binding : ActivityStrongboxLevelBinding
+    private lateinit var sensorManager: SensorManager
+    private lateinit var arrow: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStrongboxLevelBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        arrow = binding.arrow
 
         startCompass()
     }
 
     private fun startCompass(){
 
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
 
+        // Specify the sensor you want to listen to
+        sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)?.also { gyroscope ->
+            sensorManager.registerListener(
+                this,
+                gyroscope,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        // Checks for the sensor we have registered
+        if (event?.sensor?.type == Sensor.TYPE_GYROSCOPE) {
+            val zRotation = event.values[2]
+
+            arrow.rotation -= zRotation / 5F
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
     }
 
 }
