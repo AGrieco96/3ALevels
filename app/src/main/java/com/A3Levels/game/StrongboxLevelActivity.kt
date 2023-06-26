e5fb75540385b76b222229ea0e54967768ffe3f4
ditommaso1801802 <ditommaso.1801802@studenti.uniroma1.it>
2023-06-26 19:06:46 +0200

    logic implemented

--- app/src/main/java/com/A3Levels/game/StrongboxLevelActivity.kt
+++ app/src/main/java/com/A3Levels/game/StrongboxLevelActivity.kt
@@ -9,12 +9,19 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.A3Levels.databinding.ActivityStrongboxLevelBinding
+import java.util.Timer
+import java.util.TimerTask
 
 class StrongboxLevelActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var binding : ActivityStrongboxLevelBinding
     private lateinit var sensorManager: SensorManager
     private lateinit var arrow: ImageView
+    private val finalGrade = 210F
+    private var flag : Boolean = true
+    private var grade = 0F
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,13 +49,52 @@ class StrongboxLevelActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
+
         // Checks for the sensor we have registered
         if (event?.sensor?.type == Sensor.TYPE_GYROSCOPE) {
             val zRotation = event.values[2]
 
             arrow.rotation -= zRotation / 5F
+
+            if (arrow.rotation < 0){
+                grade = 360F + arrow.rotation
+            } else {
+                grade = arrow.rotation
+            }
+
+            if((finalGrade - 10F <= grade) && (grade <= finalGrade + 10F) && flag){
+                println("CIAOOOOOOOOOOOOO")
+                flag = false
+                startTimer()
+            }
+
+
+
         }
     }
+
+    //Funzione da avviare nel momento in cui la freccia entra nella threshold del valore (threshold più piccola per evitare falsi negativi)
+    private fun startTimer() {
+        var timer : Timer = Timer()
+        var secondsPassed : Int = 0
+
+        timer?.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                    if (!((finalGrade - 10F <= grade) && (grade <= finalGrade + 10F))) {
+                        timer.cancel()
+                        flag = true
+                        return
+                    }
+                    if(secondsPassed < 3) {
+                        secondsPassed++
+                    } else {
+                        println("HAI VINTO")
+                        timer.cancel()
+                    }
+            }
+        }, 0, 1000) // 1000 milliseconds = 1 second
+    }
+
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         return
